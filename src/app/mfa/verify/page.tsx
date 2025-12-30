31d24a4d2822fe271b46d3844274dc37a3a9ca48
Damien <damienbriggs@gmail.com>
2025-12-30 15:27:30 +0000

    Fix error in build

--- src/app/mfa/verify/page.tsx
+++ src/app/mfa/verify/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import { MfaVerifyForm } from '@/components/ui/access/mfa-verify-form';
 
 export default function Page() {
   return (
     <div className='flex min-h-svh w-full items-center justify-center p-6 md:p-10'>
       <div className='w-full max-w-sm'>
-        <MfaVerifyForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <MfaVerifyForm />
+        </Suspense>
       </div>
     </div>
   );
